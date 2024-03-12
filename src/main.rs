32e6c050e0f207881d98c82e575e214582a9445b
Serge Malo <sergemalo2000@outlook.com>
2024-03-12 11:42:01 -0400

    Print program header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+
+const WHITE_MAN: char = '\u{26C0}';
+const BLACK_MAN: char = '\u{26C1}';
+const WHITE_KNIGHT: char = '\u{26C2}';
+const BLACK_KNIGHT: char = '\u{26C3}';
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("SM-Checkers v{} - {} {} {} {}",  env!("CARGO_PKG_VERSION"), WHITE_MAN, BLACK_MAN, WHITE_KNIGHT, BLACK_KNIGHT);
 }

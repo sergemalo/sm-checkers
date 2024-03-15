a13e978f512a74ff3e622f4caec19ac1eea38292
Serge Malo <sergemalo2000@outlook.com>
2024-03-15 12:53:11 -0400

    Created Board class with BoardObserver trait

---
+++ src/board.rs
@@ -0,0 +1,82 @@
+use std::{cell::RefCell, rc::Rc};
+
+#[derive(Debug, Copy, Clone)]
+enum TileState {
+    Empty,
+    RedMan,
+    RedKing,
+    BlackMan,
+    BlackKing
+}
+
+#[derive(Debug)]
+struct BoardContent {
+    tiles: [TileState; 32]
+}
+
+
+impl BoardContent {
+    fn new() -> Self {
+        let mut temp_tiles: [TileState; 32] = [TileState::Empty; 32];
+        for i in 0..12 {
+            temp_tiles[i] = TileState::BlackMan;
+            temp_tiles[i+20] = TileState::RedMan;
+        }
+        BoardContent {
+            tiles: temp_tiles
+        }
+    }
+}
+
+// Define the Observer trait
+trait BoardObserver {
+    fn update(&self, b: &BoardContent);
+}
+
+// Define the Subject trait
+trait Subject {
+    fn register_observer(&mut self, bo: Rc<RefCell<dyn BoardObserver>>);
+    fn remove_observer(&mut self, bo: Rc<RefCell<dyn BoardObserver>>);
+    fn notify_observers(&self);
+}
+
+pub struct Board {
+    observers: Vec<Rc<RefCell<dyn BoardObserver>>>,
+    board_name: String,
+    board_tiles: BoardContent
+}
+
+impl Board {
+    pub fn new() -> Self {
+        Board {
+        observers: Vec::new(),
+        board_name: String::from("ZE Board"),
+        board_tiles: BoardContent::new()
+        }
+    }
+
+    pub fn doit(&self) {
+        self.notify_observers();
+    }
+}
+
+
+impl Subject for Board {
+    fn register_observer(&mut self, bo: Rc<RefCell<dyn BoardObserver>>) {
+        self.observers.push(bo);
+    }
+
+    fn remove_observer(&mut self, bo: Rc<RefCell<dyn BoardObserver>>) {
+        let index = self.observers.iter().position(|o| Rc::ptr_eq(o, &bo));
+
+        if let Some(index) = index {
+            self.observers.remove(index);
+        }
+    }
+
+    fn notify_observers(&self) {
+        for observer in self.observers.iter() {
+            observer.borrow().update(&self.board_tiles);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 use crate::checkers_ui::CheckersUi;
+use crate::board::Board;
 
+pub mod board;
 pub mod checkers_ui;
 pub mod checkers_ui_text;
 
 
 fn main() {
+
+
+
     let gui = checkers_ui_text::CheckersUiText {};
     gui.splash_screen();
 
     // Create game
+    // Create Board
+    let mut board = Board::new();
+    board.doit();
+    // Create UI
+    // Create Players
+    // Add UI and Players to the Board's Observers
 }
 
 /*
